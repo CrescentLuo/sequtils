7cdd5f2c6a61742354f0130bc356cf2bd28bfd63
CrescentLuo <crescentluozheng@gmail.com>
2021-07-13 15:42:38 -0400

    funtion vectorized

--- parseVT.py
+++ parseVT.py
@@ -2,6 +2,7 @@ import os.path
 import pandas as pd
 import argparse
 import swifter
+import numpy as np
 
 """
 # Quality Score Column
@@ -54,7 +55,7 @@ def check_other_thres(score):
 
 
 
-def clean_AS(event, sample_list, min_prop=0.5):
+def clean_AS(event, sample_list, min_prop):
     complex = event['COMPLEX']
     is_ce = complex in ['S', 'C1', 'C2', 'C3', 'ANN']
     is_ir = complex in ['IR']
@@ -62,13 +63,12 @@ def clean_AS(event, sample_list, min_prop=0.5):
     check_complex = is_ce or is_ir or is_other
     if check_complex:
         if is_ce:
-            check_list = event[sample_list].apply(check_ce_thres)
+            check_prop = np.vectorize(check_ce_thres)(event[sample_list]).sum() / len(sample_list)
         if is_ir:
-            check_list = event[sample_list].apply(check_ir_thres)
+            check_prop = np.vectorize(check_ir_thres)(event[sample_list]).sum() / len(sample_list)
         if is_other:
-            check_list = event[sample_list].apply(check_other_thres)
-        check_prop = sum(check_list) / len(sample_list) >= min_prop
-        return check_prop
+            check_prop = np.vectorize(check_other_thres)(event[sample_list]).sum() / len(sample_list)
+        return check_prop >= min_prop
     else:
         return check_complex
 
@@ -90,19 +90,29 @@ if __name__ == '__main__':
                     formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-i', '--input', required=True, help='vast-tools align output')
     parser.add_argument('-o', '--output', help='filtered output')
-    # parser.add_argument('-s', '--species',
-    #                   choices=['Hg19', 'Hs2', 'Mm9', 'Mm10'])
+    parser.add_argument('-s', '--sample', help='provaide a subgroup of samples to filter with, samples sperated by comma, e.g "sample1,sample2,sample3"')
+    parser.add_argument('-p', '--minPr', type=float, default=0.5, help='min proportion of samples with good coverage')
 
     args = parser.parse_args()
 
     as_profile = pd.read_table(args.input)
+    sub_sample_list = list(as_profile.columns[0:6])
+    if args.sample:
+        for s in args.sample.split(','):
+            sub_sample_list.append(s)
+            sub_sample_list.append(s+'-Q')
+
+        as_profile = as_profile[sub_sample_list]
+    
     sample_list = as_profile.columns[7::2]
+    min_prop = args.minPr
     print("Table containing {} sample(s) and {} event(s) for each sample:".format(len(sample_list), as_profile.shape[0]))
     print("including: {}".format(",".join(sample_list)))
-    as_profile_filtered = as_profile[as_profile.swifter.apply(clean_AS, sample_list=sample_list,axis=1)]
+    as_profile_filtered = as_profile[as_profile.swifter.apply(clean_AS, sample_list=sample_list, min_prop=min_prop,axis=1)]
     
     print("{} event(s) passed the filtering.".format(as_profile_filtered.shape[0]))
     if not args.output:
         file_name = os.path.splitext(args.input)
         args.output = file_name[0] + '_filtered' + file_name[1]
+
     as_profile_filtered.to_csv(args.output, sep="\t", index=False)
