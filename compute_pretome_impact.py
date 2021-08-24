9ec43da6eea1b60f5c18beb33a0b1ed5c412f15b
CrescentLuo <crescentluozheng@gmail.com>
2021-08-24 15:37:50 -0400

    Add cds coodinates to transcripts

--- compute_pretome_impact.py
+++ compute_pretome_impact.py
@@ -1,143 +1,149 @@
 import argparse
-import pybedtools
 import bisect
 import twobitreader
 from operator import itemgetter, attrgetter
+import swifter
+import os
+import pandas as pd
+from tqdm import tqdm
+import cgranges as cr
+import dask
+
 
 def get_args():
-    parser = argparse.ArgumentParser(description='Compute the impact of novel exons')
-    parser.add_argument('-g', '--gtf', required=True, help='input annotation file in GTF format')
-    parser.add_argument('-b', '--bed', required=True, help='table of novel exons in BED format')
-    parser.add_argument('--genome', required=True, help='genome sequence file in 2bit format')
+    parser = argparse.ArgumentParser(
+        description='Compute the impact of novel exons')
+    parser.add_argument(
+        '-d', '--gtfdir', required=True,
+        help='input annotation files in GTF format')
+    parser.add_argument(
+        '-b', '--bed', required=True,
+        help='table of novel exons in BED format')
+    parser.add_argument('--genome', required=False,
+                        help='genome sequence file in 2bit format')
     args = parser.parse_args()
     return args
 
-class AnnoGTF:
-    def __init__(self):
-        self.gene_set = set()
-        self.gene_id_dict = dict()
-        self.gene_ts_dict = dict()
-        self.transcript_exon_dict = dict()
-        self.transcript_cds_dict = dict()
-        self.gene_set = dict()
-        self.transcript_set = dict()
-        self.exon_set = dict()
-
-    def init(self,file):
-        iterms = pybedtools.BedTool(file)
-        gene_set = iterms.filter(lambda iterm: iterm.fields[2] == 'gene')
-        transcript_set = iterms.filter(lambda iterm: iterm.fields[2] == 'transcript')
-        cds_set = iterms.filter(lambda iterm: iterm.fields[2] == 'CDS')
-        exon_set = iterms.filter(lambda iterm: iterm.fields[2] == 'exon')
-        
-
-        for gene in gene_set:
-            gene_id = gene.attrs['gene_id']
-            gene_symbol = gene.attrs['gene_name']
-            self.gene_id_dict[gene_id] = gene_symbol
-            self.gene_id_dict[gene_symbol] = gene_id
-            self.gene_ts_dict[gene_id] = set()
-            self.gene_set[gene_id] = gene
-
-        for ts in transcript_set:
-            ts_id = ts.attrs['transcript_id']
-            gene_id = ts.attrs['gene_id']
-            self.gene_ts_dict[gene_id].add(ts_id)
-            self.transcript_exon_dict[ts_id] = set()
-            self.transcript_set[ts_id] = ts
-            self.transcript_cds_dict[ts_id] = list()
-
-        for exon in exon_set:
-            ts_id = exon.attrs['transcript_id']
-            exon_id = exon.attrs['exon_id']
-            self.transcript_exon_dict[ts_id].add(exon_id)
-            self.exon_set[exon_id] = exon
-        
-        for cds in cds_set:
-            ts_id = cds.attrs['transcript_id']
-            self.transcript_cds_dict[ts_id].append(cds)
-            
-        
-        return self
-    
-    def get_ts(self, gene_id):
-        try:
-            return self.gene_ts_dict[gene_id]
-        except:
-            return
-    
-
-def read_gtf(gtf_file):
-    gtf = AnnoGTF().init(file=gtf_file)
-    #subset = a.filter(lambda b: b.chrom == 'chr1' and b.start < 150)
-    
-    return gtf
-
-def read_exon(exon_file):
-    exon = pybedtools.BedTool(exon_file)
-    return exon
 
 def search_stop_codon(seq, lo=0):
     # stop codon TAG TAA TGA
     stop_codon = set("TAG", "TAA", "TGA")
-    for i in range(0,len(seq),3):
+    for i in range(0, len(seq), 3):
         codon = seq[i:i+2]
         if codon in stop_codon:
             return i, codon
-    return -1,"NA"
+    return -1, "NA"
 
 
-
-
-def track_ts(exon, gtf, genome):
-    gene = exon.name.split("_")[1]
-    print(gene)
-    gene_id = gtf.gene_id_dict[gene]
-    ts_set = gtf.get_ts(gene_id)
-    if exon.strand == '+':
-        exon_seq = genome[exon.chrom][exon.start:exon.end]
-    else:
-
-    for ts_id in ts_set:
-        ts = gtf.transcript_set[ts_id]
-        cds_set_sort = list()
-        if exon.start >= ts.start and exon.end <= ts.end:
-            cds_set = gtf.transcript_cds_dict[ts_id]
-            cds_set = sorted(cds_set)
-            pos = -1
-            cds_seq = ''
-            for idx, cds in enumerate(cds_set):
-                if cds > exon:
-                    pos = idx
-                    residue = len(cds_seq) % 3
-                    if residue:
-                        residue_seq = cds_seq[-residue::]
-                        exon_seq_with_overhang = residue_seq + exon_seq[::residue-3]
-                        
-                    else:
-                    
-                    pos, codon = search_stop_codon(exon_seq_with_overhang)
-                    if pos != -1:
-                    # disruption
-                        print pos, codon
-                        return
-                     
-
-
+def track_ts(exon, cr_ts, gtf_ts, gtf_exon, gtf_ts_exon, gtf_ts_cds, gtf_cds):
+    exon_s = exon['start']
+    exon_e = exon['end']
+    exon_chrom = exon['chrom']
+    exon_strand = exon['strand']
+    # get the corresponding gene
+    for ts_s, ts_e, ts_idx in cr_ts.overlap(
+            exon_chrom, exon_s, exon_e):
+        transcript_id = gtf_ts[ts_idx]['transcript_id']
+        ts_strand = gtf_ts[ts_idx]['strand']
+        if exon_strand != ts_strand:
+            continue
+        # 检测target 是否在coding region
+        
+        c1_idx = -1
+        c2_idx = -1
+        last_eej_idx = -1
+        for idx in gtf_ts_exon[transcript_id]:
+            ts_exon_s = gtf_exon[idx]['start']
+            ts_exon_e = gtf_exon[idx]['end']
+            #print("{}\t{}<--{}-->{}\t{}".format(ts_exon_s,ts_exon_e,exon_strand,exon_s,exon_e))
+            if exon_strand == '+':
+                last_eej_idx = idx
+            else:
+                if last_eej_idx == -1:
+                    last_eej_idx =idx
+            if ts_exon_e < exon_s:
+                if exon_strand == '+':
+                    c1_idx = idx
                 else:
-                    cds_seq = cds_seq + genome[cds.chrom][cds.start:cds.end]
-                    
+                    c2_idx = idx
+            if ts_exon_s > exon_e:
+                if exon_strand == '+':
+                    c2_idx = idx
+                else:
+                    c1_idx = idx
+        if c1_idx != -1 and c2_idx !=-1:
 
-        
-        
+            # 
+    return -1, -1
 
+
+def get_dis_last_eej():
+    return
+
+
+def get_cds():
+    return
+
+
+def search_ptc():
+    return
+
+
+def build_cgranges(df):
+    g = cr.cgranges()
+    for idx, row in tqdm(df.iterrows(), total=df.shape[0]):
+        seqname = row['seqname']
+        start = row['start']
+        end = row['end']
+        g.add(seqname, start, end, idx)
+    g.index()
+    return g
+
+def build_ts_exon_dict(df):
+    ts_exon_dict = dict()
+    
+    for idx in df:
+        transcript_id = df[idx]['transcript_id']
+        if transcript_id not in ts_exon_dict:
+            ts_exon_dict[transcript_id] = list()
+        ts_exon_dict[transcript_id].append(idx)
+    return ts_exon_dict
 
 if __name__ == '__main__':
     args = get_args()
-    gtf = read_gtf(args.gtf)
-    exons = read_exon(args.bed)
-    genome = twobitreader.TwoBitFile(args.genome)
-    for e in exons:
-        track_ts(e, gtf, genome)
+    print(args.gtfdir)
+    gtf_ts = pd.read_csv(
+        os.path.join(
+            args.gtfdir, 'gencode.v31.primary_assembly.annotation.ts.tab'),
+        sep='\t', dtype={'start': int, 'end': int})
+    gtf_ts = gtf_ts[gtf_ts['gene_type'] == 'protein_coding']
+    gtf_ts = gtf_ts[gtf_ts['transcript_support_level'].isin(['1',
+                                                             '2'])]
+    cr_ts = build_cgranges(gtf_ts)
+    gtf_ts = gtf_ts.to_dict('index')
+
+    gtf_exon = pd.read_csv(
+        os.path.join(
+            args.gtfdir, 'gencode.v31.primary_assembly.annotation.exon.tab'),
+        sep='\t', dtype={'start': int, 'end': int})
+    gtf_exon = gtf_exon.to_dict('index')
+    gtf_ts_exon = build_ts_exon_dict(gtf_exon)
+    gtf_cds = pd.read_csv(
+        os.path.join(
+            args.gtfdir, 'gencode.v31.primary_assembly.annotation.CDS.tab'),
+        sep='\t', dtype={'start': int, 'end': int})
+    gtf_cds = gtf_cds.to_dict('index')
+    gtf_ts_cds = build_ts_exon_dict(gtf_cds)
     
-    
+    #genome = twobitreader.TwoBitFile(args.genome)
+    exon_target = pd.read_csv(
+        args.bed, sep='\t',
+        names=['chrom', 'start', 'end', 'name', 'score', 'strand'])
+    for idx, exon in tqdm(exon_target.iterrows(), total=exon_target.shape[0]):
+    #for idx, exon in exon_target.iterrows():
+        c1_idx, c2_idx = track_ts(exon, cr_ts=cr_ts, gtf_ts=gtf_ts, gtf_exon=gtf_exon, gtf_ts_exon=gtf_ts_exon, gtf_ts_cd=gtf_ts_cds, gtf_cds=gtf_cds)
+        if c1_idx != -1 and c2_idx != -1:
+            print("##################")
+            print(gtf_exon[c1_idx])
+            print(exon)
+            print(gtf_exon[c2_idx])
--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-import pandas
+import pandas as pd
 import swifter
 import re
 def reverse_complement(seq):
@@ -14,6 +14,7 @@ def reverse_complement(seq):
 def parse_gtf_attrs(feature):
     feature_type = feature['feature']
     attrs = feature['attribute']
+    feature_start = feature['start']
     regex = r"(\S+\s\S+;)"
     attrs_dict = dict()
     for attr in re.finditer(regex,attrs):
@@ -33,6 +34,8 @@ def parse_gtf_attrs(feature):
         feature['gene_id'] = attrs_dict['gene_id']
         feature['gene_type'] = attrs_dict['gene_type']
         feature['gene_name'] = attrs_dict['gene_name']
+        feature['cds_start'] = feature_start
+        feature['cds_end'] = feature_start
         feature['transcript_id'] = attrs_dict['transcript_id']
         if 'transcript_support_level' in attrs_dict:
             feature['transcript_support_level'] = attrs_dict['transcript_support_level']
@@ -46,7 +49,7 @@ def parse_gtf_attrs(feature):
         feature['exon_id'] = attrs_dict['exon_id']
         feature['exon_number'] = int(attrs_dict['exon_number'])
     if feature_type == 'CDS':
-        feature['gene_id'] = attrs_dict['gene_id']
+        feature['gene_id'] = attrs_dict['gene_id'] 
         feature['gene_type'] = attrs_dict['gene_type']
         feature['gene_name'] = attrs_dict['gene_name']
         feature['transcript_id'] = attrs_dict['transcript_id']
@@ -55,10 +58,37 @@ def parse_gtf_attrs(feature):
 
     return feature
 
-
+def assign_cds_to_ts(gtf_ts, gtf_cds):
+    gtf_ts = gtf_ts.to_dict('index')
+    gtf_cds = gtf_cds.to_dict('index')
+    ts_cds_dict = dict()
+    
+    for idx in gtf_cds:
+        transcript_id = gtf_cds[idx]['transcript_id']
+        if transcript_id not in ts_cds_dict:
+            ts_cds_dict[transcript_id] = list()
+        ts_cds_dict[transcript_id].append(idx)
+    for idx in gtf_ts:
+        transcript_id = gtf_ts[idx]['transcript_id']
+        for cds_idx in ts_cds_dict[transcript_id]:
+            cds_start = int(gtf_cds[cds_idx]['start'])
+            cds_end = int(gtf_cds[cds_idx]['end'])
+            ts_cds_start = int(gtf_ts[idx]['cds_start'])
+            ts_cds_end = int(gtf_ts[idx]['cds_end'])
+            ts_start = int(gtf_ts[idx]['start'])
+            if ts_cds_start == ts_start:
+                gtf_ts[idx]['cds_start'] = cds_start
+                gtf_ts[idx]['cds_end'] = cds_end
+            else:
+                if cds_start < ts_cds_start:
+                    gtf_ts[idx]['cds_start'] = cds_start
+                if cds_end > ts_cds_end:
+                    gtf_ts[idx]['cds_end'] = cds_end
+    gtf_ts = pd.DataFrame.from_dict(gtf_ts, orient='index')
+    return gtf_ts
 
 def parse_gtf(gtf_file):
-    gtf = pandas.read_csv(gtf_file, sep='\t', comment='#', names=['seqname','source','feature','start','end','score','strand','frame','attribute'])
+    gtf = pd.read_csv(gtf_file, sep='\t', comment='#', names=['seqname','source','feature','start','end','score','strand','frame','attribute'])
     gtf_gene = gtf[gtf['feature']=='gene'].swifter.apply(parse_gtf_attrs,axis=1)
     gtf_transcript = gtf[gtf['feature']=='transcript'].swifter.apply(parse_gtf_attrs,axis=1)
     gtf_exon = gtf[gtf['feature']=='exon'].swifter.apply(parse_gtf_attrs,axis=1)
